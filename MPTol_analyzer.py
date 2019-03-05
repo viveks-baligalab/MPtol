ce315d8c9dc8fd9375da9ea86a309f6edef12d23
Vivek Srinivas <46542225+viveks-baligalab@users.noreply.github.com>
2019-03-05 14:16:06 -0800

    Add files via upload
     - Changed read function for BioTek data to incorporate different BioTek data formats
    _ Included an input for separately attribute

--- MPTol_analyzer.py
+++ MPTol_analyzer.py
@@ -9,7 +9,7 @@ Program requires two files - 'Labels.csv' and 'Readings.txt' for example
             plot_growth_in_wells(results_text_file, labels, instrument = "BioTek"/"BioAnalyzer")
     Step 2: Identify OD threshold for 'Start of growth point determination'
     Step 3: Calculate drug tolerance with command -
-            calculate_plot_tolerance(results_text_file, labels, ODT = __,drug = __, instrument = "BioTek"/"BioAnalyzer" )
+            calculate_plot_tolerance(results_text_file, labels, ODT = __, seperateby = "drug"/"type", drug/type = __, instrument = "BioTek"/"BioAnalyzer" )
 """
 
 import csv, re
@@ -32,14 +32,22 @@ def brkstr(string):
 
 def brkstr2(string):
     return string.split
+
+def convert_to_strings(listx):
+	new_list = []
+	for i in listx:
+		new_list.append(str(i))
+	return new_list
    
 
 def read_results_biotek(results_text_file):
     with open(results_text_file,"r") as f:
         lines = [ i[:-2] for i in f.readlines()]
-    data1 = lines[lines.index("OD600:600"):]
-    data = data1[:data1.index("Results")]
-    data_split = np.array([i.split("\t") for i in data[2:-1]]).T
+    p1, p2 = re.compile("Time"), re.compile("Result")
+    idp1, idp2 = list(filter(p1.match,lines)),list(filter(p2.match,lines))    
+    idx1, idx2 = lines.index(idp1[-1]), lines.index(idp2[-1])
+    data = lines[idx1:idx2-1]
+    data_split = np.array([i.split("\t") for i in data]).T
     Time_points = [str(get_mins(i)) for i in data_split[0][1:]]
     od_readings = data_split[2:]
     ork = len([i for i in od_readings[0] if i != ""])
@@ -47,9 +55,10 @@ def read_results_biotek(results_text_file):
 
 def read_results_bioanalyzer(results_text_file):
     data = pd.read_csv(results_text_file,header=0).transpose()
-    time = data[0,:].values
-    array = [[i]+list(l.values) for i,l in data.iloc[1:].iterrows()]
-    return time, array
+    time = data.iloc[0,:].values
+    Time_points = [str(get_mins(i)) for i in time]
+    od_readings = [convert_to_strings([i]+list(l.values)) for i,l in data.iloc[1:,:].iterrows()]
+    return od_readings, Time_points
 
 def read_results_text_file(results_text_file, labels, instrument_type):
     labels = pd.read_csv(labels,index_col=0,header = 0)
@@ -90,10 +99,10 @@ def get_sgt(od,time, odt):
     model = np.polyfit(od,time,2)
     det_model = np.poly1d(model)
     sgt = det_model(odt)
-    if sgt > min(time) and sgt < max(time)+np.mean(time):
+    if sgt > min(time) and sgt < 2.0*(max(time)):#:+np.mean(time):
         ret_sgt = sgt
-    elif sgt > max(time)+np.mean(time):
-        ret_sgt = 48.0
+    elif sgt > 2.0*(max(time)):#:+np.mean(time):
+        ret_sgt = 2.0*(max(time))#:+np.mean(time)
     else:
         ret_sgt = 0.5
     return ret_sgt
@@ -125,37 +134,60 @@ def plot_sgt_in_wells(results_text_file,labels,ODT, **kwargs):
 def calculate_tolerance(results_text_file,labels,ODT, instrument):
     data = add_sgt(results_text_file,labels,ODT, instrument)
     data = data.drop(["Time_points","OD"], axis =1)
-    control = np.nanmean(data[data.Sample_type.isin(["Control"])].SGT.values)
-    if control < 0:
-        control = 48.0
-    else:
+    control_sgts = data[data.Sample_type.isin(["Control"])].SGT.values
+    control = np.nanmean(control_sgts)
+    if control > 0: 
         control = control
+    else:
+        control = max(control_sgts)
+    print "control SGT = %s"%control
     for n,i in data.iterrows():
-        data.at[n,"log2_surviving_cells"] = np.log2(2**(-(control-i.SGT)))
+        if i.SGT < control:
+            data.at[n,"log2_surviving_cells"] = np.log2(control-i.SGT)
+        else:
+            data.at[n,"log2_surviving_cells"] = 0.0
     return data
         
-def calculate_plot_tolerance(results_text_file,labels,**kwargs):
+def calculate_and_plot_tolerance(results_text_file,labels,**kwargs):
     """Requires 'results text/csv file', 'labels csv file', ODT[optional], Drug and instrument type.
     -- Use this to calculate and plot % cell survival"""
     if "ODT" not in kwargs.keys():
         kwargs["ODT"] = 0.15
     else:
         pass
-    if "drug" not in kwargs.keys():
-        print "Provide drug name"
+    if "seperateby" not in kwargs.keys():
+        print "Provide an attribute to segregate data"
     else:
-        data = calculate_tolerance(results_text_file,labels,kwargs["ODT"], kwargs["instrument"])
-        if kwargs["drug"] not in data.Drug.values:
-            print "Enter correct name of the drug"
-        #elif kwargs["instrument"] not in data.Drug.values:
-            #print "Enter instrument used for reading OD ('BioTek'/'BioAnalyzer')"
-        else:
-            data = data[data.Drug.isin([kwargs["drug"]])]
-            tolerance = data.pivot(index="Incubation_time",columns="Concentration",values="log2_surviving_cells")
-            sns.heatmap(tolerance, cmap="RdYlGn", linewidths=0.30,cbar_kws={'label': 'log2 (Fraction of surviving cells)'})
-            plt.title("%s treatment profile"%kwargs["drug"])
-            plt.savefig("%s_%s.pdf"%(results_text_file.split(".")[0],kwargs['drug']))
-            plt.clf()
-            data.to_csv("%s_%s_analyzed.csv"%(results_text_file.split(".")[0],kwargs['drug']))
+        if kwargs["seperateby"] in ["drugs", "drug","Drug","Drugs"]:
+            if "drug" not in kwargs.keys():
+                print "Provide drug name"
+            else:
+                data = calculate_tolerance(results_text_file,labels,kwargs["ODT"], kwargs["instrument"])
+                if kwargs["drug"] not in data.Drug.values:
+                    print "Enter correct name of the drug"
+                else:
+                    data = data[data.Drug.isin([kwargs["drug"]])]
+                    tolerance = data.pivot(index="Incubation_time",columns="Concentration",values="log2_surviving_cells")
+                    sns.heatmap(tolerance, cmap="RdBu", vmin = 0,vmax =12, linewidths=0.30,cbar_kws={'label': 'log2 (Fraction of surviving cells)'})
+                    plt.title("%s treatment profile"%kwargs["drug"])
+                    plt.savefig("%s_%s.pdf"%(results_text_file.split(".")[0],kwargs['drug']))
+                    plt.clf()
+                    data.to_csv("%s_%s_analyzed.csv"%(results_text_file.split(".")[0],kwargs['drug']))
+        elif kwargs["seperateby"] in ["sample", "type","sampletype","sample_type"]:
+            if "type" not in kwargs.keys():
+                print "Provide the sample type"
+            else:
+                data = calculate_tolerance(results_text_file,labels,kwargs["ODT"], kwargs["instrument"])
+                if kwargs["type"] not in data.Sample_type.values:
+                    print "Enter the correct sample type"
+                else:
+                    data = data[data.Sample_type.isin([kwargs["type"]])]
+                    tolerance = data.pivot(index="Incubation_time",columns="Concentration",values="log2_surviving_cells")
+                    sns.heatmap(tolerance, cmap="RdBu",vmin=0,vmax=12, linewidths=0.30,cbar_kws={'label': 'log2 (Fraction of surviving cells)'})
+                    plt.title("%s treatment profile of %s"%(data.Drug.values[0],kwargs['type']))
+                    plt.savefig("%s_%s.pdf"%(results_text_file.split(".")[0],kwargs['type']))
+                    plt.clf()
+                    data.to_csv("%s_%s_analyzed.csv"%(results_text_file.split(".")[0],kwargs['type']))
+                
 
     
